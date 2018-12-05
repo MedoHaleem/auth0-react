11dd5a41be8ed36811b55989c1b73ad18095d7d1
Medo Haleem <medo_e3@hotmail.com>
2018-12-05 14:02:43 +0200

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Route} from "react-router-dom"
+import Home from './Home'
+import Profile from './Profile'
+import Nav from './Nav'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return <>
+            <Nav />
+            <div className="body">
+            <Route path="/" component={Home} />
+            <Route path="/profile" component={Profile} />
+            </div>
+        </>
+    }
 }
 
 export default App;
