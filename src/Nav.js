9eec0c177a8c72d51c94e43e99fe0709faf3a18b
Medo Haleem <medo_e3@hotmail.com>
2018-12-06 14:29:02 +0200

    added logout

--- src/Nav.js
+++ src/Nav.js
@@ -4,19 +4,24 @@ import logo from './logo.svg';
 
 class Nav extends Component {
     render() {
+        const {isAuthenticated, login, logout} = this.props.auth;
         return (
             <nav>
-            <ul>
-                <li>
-                    <img src={logo}  className="logo-nav" alt="logo"/>
-                </li>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-            </ul>
+                <ul>
+                    <li>
+                        <img src={logo} className="logo-nav" alt="logo"/>
+                    </li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                    <li>
+                        <a href="#" onClick={isAuthenticated() ? logout : login}>
+                            {isAuthenticated() ? " Log Out" : "Log In"} </a>
+                    </li>
+                </ul>
             </nav>
         );
     }
